b2d422e1ee2c9807d5d469f099684b33a61b6b81
kanu0158 <kanu0158@naver.com>
2018-09-11 10:38:17 +0900

    convert lamda

--- src/main/java/com/gms/web/generic/GenericIntro.java
+++ src/main/java/com/gms/web/generic/GenericIntro.java
@@ -6,13 +6,24 @@ public class GenericIntro {
  class Member{} -> static 상태로 Member 타입을 생성
  List<Member>
  <-> Dynamic한 타입생성
-  
+  왜쓰냐?
+   캐스팅 안하려고
+   장점. 타입의 안전성 제공, 형변환 생략
  * **/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			//dkdkd준이바보다
-				//안아링니ㅏ
-		//ㅇㄴㅁㄹㅇㄴㅁㄹㅇㄴㄹ
+		
+		
+	}
+	class Box{
+		Object item;
+
+		public Object getItem() {
+			return item;
+		}
+
+		public void setItem(Object item) {
+			this.item = item;
+		}
 	}
 
 }
